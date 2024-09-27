1d4434608049c096c9bb5d9a44c2554e1c3f02f6
Ritesh Nagrikar <riteshnagrikar77@gmail.com>
2024-09-27 12:22:43 +0530

    updated the files

--- server.js
+++ server.js
@@ -3,21 +3,73 @@ const app = express();
 const db = require('./db');
 require('dotenv').config();  // URL .env me hai ab
 
+
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+
+const Person = require('./Models/Person')
+
+
 const bodyParser = require('body-parser'); // npm i body-parser
 app.use(bodyParser.json());             // json data bhjre 
-
-
-
 
 const PORT = process.env.PORT || 3000; // q ki port ab .env me hai
 
 
 
-app.get('/', function (req, res) {
+
+
+
+//MIDDLEWARE FUNCTION
+const logRequest = (req,res,next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next();
+}
+
+app.use(logRequest); // APPLY FOR ALL ENDPOINTS BY THIS WAY
+
+
+
+
+
+
+// PASSPORT , USERNAME, PASSWORD
+
+passport.use(new LocalStrategy(async (USERNAME, password, done)=>{
+    // authentication logic here
+    try{
+        console.log('Received credentials:'. USERNAME, password);
+        const user = await Person.findOne({username: USERNAME});
+        if(!user)
+            return done(null, false, {message: 'Incorrect username'});
+
+        const isPasswordMatch = user.password === password ? true : false;
+        if(isPasswordMatch){
+            return done(null, user);
+        }
+        else{
+            return done(null, false,{message: 'Incorrect password'});
+        }
+
+    }
+    catch(err){
+        return done(err);
+
+    }
+}))
+
+
+
+app.use(passport.initialize());
+
+
+
+
+app.get('/',passport.authenticate('local', {session: false}),function (req, res) {
     res.send('Welcome to our HOTEL');
 })
-
-
 
 
 
@@ -29,7 +81,7 @@ const menuItemRoutes = require('./routes/menuItemRoutes');    // menuItemRoutes 
 
 // Use the routers
 app.use('/person',personRoutes);  // we used here /person 
-app.use('/menu',menuItemRoutes); // // we used here /menu 
+app.use('/menu', menuItemRoutes); // // we used here /menu 
 
 
 
